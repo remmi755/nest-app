2ac257ac09a96985e54f18cb10df4e7c8c15d722
remmi755 <90457721+remmi755@users.noreply.github.com>
2024-04-15 16:52:21 +0300

    Initial commit

---
+++ src/schemas/User.schema.ts
@@ -0,0 +1,24 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop({ unique: true })
+  id: number;
+
+  @Prop()
+  email: string;
+
+  @Prop()
+  first_name: string;
+
+  @Prop()
+  last_name: string;
+
+  @Prop()
+  avatar: string;
+}
+
+export const CreateUserSchema = SchemaFactory.createForClass(User);
